26ad7499135450ade0fc0eab50c768925b6f2ceb
Gururaja.Reddy <Gururaja.Reddy@INC00752.Incture.local>
2017-12-28 16:19:55 +0530

    Removed @After metho

--- src/com/incture/proj/testNg/AnnotationTestNGImpl2.java
+++ src/com/incture/proj/testNg/AnnotationTestNGImpl2.java
@@ -20,9 +20,7 @@ public class AnnotationTestNGImpl2 {
 	}
 	@AfterMethod
 	public void afterMethod(){
-		System.out.println("i am in afterMethod()");
-		System.out.println("i am in afterMethod()");
-		System.out.println("i am in afterMethod()");
+		
 	}
 
 	@Test
